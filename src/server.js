88c1316b647e47d4c66f79a9d7019c9f6a3810dc
raphael mondo <raphaelmondo15@gmail.com>
2026-09-24 16:22:08 +0100

    feat: start monitor scheduler with server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { startScheduler } from './scheduler/monitor.scheduler.js';
 
 dotenv.config();
 
@@ -7,3 +8,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+startScheduler();
